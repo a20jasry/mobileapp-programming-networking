3d646e8a6067b3b1ae6ee9b444b4cef727f8b951
a20jasry <81618961+a20jasry@users.noreply.github.com>
2021-04-29 14:14:55 +0200

    ArrayList<Mountain> + ArrayAdapter<Mountain>

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.networking;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+    private Mountain[] mountain={new Mountain()};
+    private ArrayList < Mountain> arrayMountain;
+    private ArrayAdapter<Mountain> adapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+       adapter=new ArrayAdapter<Mountain>(this,R.layout.list_item_text_view);
     }
 }
---
+++ app/src/main/java/com/example/networking/Mountain.java
@@ -0,0 +1,8 @@
+package com.example.networking;
+
+public class Mountain {
+    private String name;
+    private String location;
+    private String height;
+
+}
