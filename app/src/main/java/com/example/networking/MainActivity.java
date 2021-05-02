0a8c492544b8b45a7a56e03036347e0fc2ade822
a20jasry <81618961+a20jasry@users.noreply.github.com>
2021-05-02 10:47:46 +0200

    JsonTask + Json web service

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -2,15 +2,29 @@ package com.example.networking;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    private Mountain[] mountain={new Mountain()};
-    private ArrayList < Mountain> arrayMountain;
-    private ArrayAdapter<Mountain> adapter;
+    private ArrayList<Mountain> mountains;
+    private ArrayAdapter<Mountain> Mountains;
+    ;
 
 
     @Override
@@ -18,6 +32,66 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-       adapter=new ArrayAdapter<Mountain>(this,R.layout.list_item_text_view);
-    }
-}
+
+        ListView my_listview = (findViewById(R.id.list_item_text_view)); // Get a reference to our ListView
+        my_listview.setAdapter(Mountains); // Connect ArrayAdapter to ListView
+        Mountains = new ArrayAdapter<Mountain>(this, R.layout.list_item_text_view); // create an ArrayAdapter
+
+        my_listview.setOnClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext("your text here ", Toast.LENGTH_LONG).show();
+            }
+        });//  OnItemClickListener
+
+        new JsonTask().execute(https://wwwlab.iit.his.se/brom/kurser/mobilprog/dbservice/admin/getdataasjson.php?type=brom);
+    } // onCreate
+
+    @SuppressLint("StaticFieldLeak")
+    private class JsonTask extends AsyncTask<String, String, String> {
+
+        private HttpURLConnection connection = null;
+        private BufferedReader reader = null;
+
+        protected String doInBackground(String... params) {
+            try {
+                URL url = new URL(params[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+
+                InputStream stream = connection.getInputStream();
+                reader = new BufferedReader(new InputStreamReader(stream));
+
+                StringBuilder builder = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null && !isCancelled()) {
+                    builder.append(line).append("\n");
+                }
+                return builder.toString();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+                try {
+                    if (reader != null) {
+                        reader.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String json) {
+            Log.d("TAG", json);
+        }//onPostExecute
+
+    } //JsonTask
+} //  MainActivity
+
--- app/src/main/java/com/example/networking/Mountain.java
+++ app/src/main/java/com/example/networking/Mountain.java
@@ -2,7 +2,18 @@ package com.example.networking;
 
 public class Mountain {
     private String name;
+    private String type;
+    private String company;
     private String location;
-    private String height;
+    private String category;
+    private String size;
+    private String cost;
 
 }
+
+public String getName(){
+    return name;
+    return size;
+}
+
+
